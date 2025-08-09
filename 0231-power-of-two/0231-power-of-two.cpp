255bcf58c8107fd82e493d3c2f6f2903066928b3
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2025-08-09 20:30:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-         return n > 0 && (n & (n - 1)) == 0;
-
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
-
-
-//alternate version
-// class Solution {
-// public:
-//     bool isPowerOfTwo(int n) {
-//         if (n <= 0) return false;
-//         while (n % 2 == 0) {
-//             n /= 2;
-//         }
-//         return n == 1;
-//     }
-// };
