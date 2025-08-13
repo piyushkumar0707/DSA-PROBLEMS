5c1998427485b0fc6ea9933a3630fce49976003b
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2025-08-13 23:27:20 +0530

    Time: 0 ms (100%), Space: 9.3 MB (86.45%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+
+public:
+    MyStack() {
+        // No need to initialize q here; it's already declared
+    }
+
+    void push(int x) {
+        int s = q.size();
+        q.push(x);
+        for (int i = 0; i < s; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
