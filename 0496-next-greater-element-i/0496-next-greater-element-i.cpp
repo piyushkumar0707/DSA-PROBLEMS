6c0c93c7e22f485b205504a38a98b1338ae0d934
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2025-08-24 17:17:36 +0530

    Time: 0 ms (100%), Space: 13 MB (15.36%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> nextGreater;
+        unordered_map<int, int> nge;
         stack<int> st;
 
-        // Traverse nums2 from right to left
         for (int i = nums2.size() - 1; i >= 0; --i) {
             int num = nums2[i];
 
-            // Maintain a decreasing stack
             while (!st.empty() && st.top() <= num) {
                 st.pop();
             }
 
-            // If stack is empty, no greater element to the right
-            nextGreater[num] = st.empty() ? -1 : st.top();
-
-            // Push current number to stack
+            nge[num] = st.empty() ? -1 : st.top();
             st.push(num);
         }
 
-        // Build the result for nums1 using the map
-        vector<int> result;
+        vector<int> ans;
         for (int num : nums1) {
-            result.push_back(nextGreater[num]);
+            ans.push_back(nge[num]);
         }
 
-        return result;
+        return ans;
     }
 };
