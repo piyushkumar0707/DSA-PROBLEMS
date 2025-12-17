b3c9394e5e8961e1e96cf6e6c3afc19c0bde471e
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2025-12-17 11:51:39 +0530

    Time: 2 ms (45.5%), Space: 19.5 MB (73.59%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,33 @@
 class Solution {
-public:
-    void dfs(int city, vector<vector<int>>& isConnected, vector<bool>& visited) {
-        visited[city] = true;
-        for (int neighbor = 0; neighbor < isConnected.size(); ++neighbor) {
-            if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
-                dfs(neighbor, isConnected, visited);
+private:
+    // The "Worker": Visits all cities in one province
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
+        visited[node] = 1;
+        
+        // Traverse the adjacency matrix row for the current node
+        for (int neighbour = 0; neighbour < isConnected.size(); neighbour++) {
+            // Check if there is a connection (1) and if we haven't visited it yet
+            if (isConnected[node][neighbour] == 1 && !visited[neighbour]) {
+                dfs(neighbour, isConnected, visited);
             }
         }
     }
 
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n, false);
+        vector<int> visited(n, 0);
         int provinces = 0;
 
-        for (int city = 0; city < n; ++city) {
-            if (!visited[city]) {
-                dfs(city, isConnected, visited);
-                ++provinces;
+        // The "Manager": Checks for disconnected provinces
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                // If we find an unvisited city, it's a new province
+                provinces++; 
+                dfs(i, isConnected, visited); // Visit the entire province
             }
         }
-
+        
         return provinces;
     }
 };
