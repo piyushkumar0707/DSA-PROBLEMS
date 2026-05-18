01f4d2cd2afeb8dd67288894b85bb5a0dbeb6541
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-05-18 21:32:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.6%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,7 +6,7 @@ public:
             int  digit = x%10 ;
              num = num*10 + digit ; 
              if ( num<INT_MIN || num>INT_MAX) return 0 ;
-             x = x/10 ;
+             x = x/10 ;  
         }
         return (int)num ;
     }
