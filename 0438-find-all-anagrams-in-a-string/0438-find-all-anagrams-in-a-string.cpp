8bb5d87e9c928491ed4f88c97e2279e94c015612
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-06-02 18:50:35 +0530

    Time: 7 ms (49.81%), Space: 11.6 MB (63.34%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int>result;
+        if(s.size()<p.size())return result;
+        int k=p.size();
+        vector<int>patFreq(26,0), winFreq(26,0);
+        for(char c:p)patFreq[c-'a']++;
+
+        for(int i=0; i<k; i++)winFreq[s[i]-'a']++;
+        if(winFreq == patFreq)result.push_back(0);
+
+        for(int i=k; i<s.size(); i++){
+            winFreq[s[i]-'a']++;
+            winFreq[s[i-k]-'a']--;
+
+            if(winFreq==patFreq)result.push_back(i-k+1);
+        }return result;
+    }
+};
