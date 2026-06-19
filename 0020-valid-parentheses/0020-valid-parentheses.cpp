d2c42c29a258db79432d1b2d401da9500dff8760
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-06-19 11:10:17 +0530

    Time: 3 ms (9.41%), Space: 10.1 MB (6.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
+        stack<int>st;
         for(char ch: s){
             if(ch=='(' || ch=='{' || ch=='[')st.push(ch);
             else{
                 if(st.empty())return false;
-                char top=st.top();
-                if((ch==')' && top!='(') ||
-                (ch=='}' && top!='{') ||
-                (ch==']' && top!='[')){
-                    return false;
-                }st.pop();
+                char top= st.top();
+                if((ch==')' && top!='('  )||(ch=='}' && top!='{'  ) ||(ch==']' && top!='['  ))return false;
+                st.pop();
             }
-        }return st.empty();
+        }
+        return st.empty();
     }
 };
