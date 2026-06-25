50ddf55b53dc2d4c6fcab7ea5448a4600f8fcd5d
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-06-25 17:58:41 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.06%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -7,33 +7,20 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- *////
-
-// recursive soln
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head==NULL || head->next==NULL){
-            return head;
-        }ListNode* newHead= reverseList(head->next);
-        ListNode*front= head->next;
-        front->next=head;
-        head->next=NULL;
-        return newHead;
+        if(head==NULL || head->next==NULL)return head;
+        ListNode* prev= NULL;
+        ListNode* curr= head;
+        while(curr!=NULL){
+            ListNode* nextNode= curr->next;
+            curr->next= prev;
+            prev=curr;
+            curr=nextNode;
+
+        }return prev;
+        
     }
 };
-
-
-// ITERATIVE SOLN
-
-Node* reverseLinkedList(Node *head)
-{
-        Node*temp =head;
-        Node*prev=NULL;
-        while(temp!=NULL){
-            Node* front= temp->next;
-            temp->next= prev;
-            prev=temp;
-            temp=front;
-        }return prev;
-}
