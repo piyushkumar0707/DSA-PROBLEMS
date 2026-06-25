0f894a7f444145937594423065db70985d0d59d7
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-06-25 16:34:16 +0530

    Time: 3 ms (59.7%), Space: 118 MB (64.83%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,32 +10,42 @@
  */
 class Solution {
 public:
-ListNode* reverseLinkedList(ListNode* head){
-    if(head==NULL || head->next==NULL)return head;
-    ListNode* newHead=reverseLinkedList(head->next);
-    ListNode* front=head->next;
-    front->next=head;
-    head->next=NULL;
-    return newHead;
-}
     bool isPalindrome(ListNode* head) {
         if(head==NULL || head->next==NULL)return true;
-        ListNode*slow=head;
-        ListNode*fast=head;
+        ListNode* middle=findMiddle(head);
+        ListNode* secondHalfHead= findReverse(middle->next);
+
+        ListNode* p1= head;
+        ListNode* p2= secondHalfHead;
+        bool result=true;
+        while(p2!=NULL){
+            if(p1->val!=p2->val){
+                return false;
+                break;
+            }p1=p1->next;
+            p2=p2->next;
+        }return result;
+        
+    }
+
+    ListNode* findMiddle(ListNode* head){
+        ListNode* fast= head;
+        ListNode* slow= head;
         while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-        }
-        ListNode *newHead=reverseLinkedList(slow->next);
-        ListNode*first=head;
-        ListNode*second=newHead;
-        while(second!=NULL){
-            if(first->val!=second->val){
-                reverseLinkedList(newHead);
-                return false;
-            }first=first->next;
-            second=second->next;
-        }reverseLinkedList(newHead);
-        return true;
+        }return slow;
+    }
+
+
+    ListNode* findReverse(ListNode* head){
+        ListNode* prev= NULL;
+        ListNode* curr= head;
+        while(curr!=NULL){
+            ListNode* nextNode= curr->next;
+            curr->next= prev;
+            prev= curr;
+            curr=nextNode;
+        }return prev;
     }
 };
