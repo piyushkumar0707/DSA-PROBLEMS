c8e68f0d5fac096582b20ecd1ca493faaceb9cbb
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-01 07:33:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-
-        long long N = n; // Use long long to avoid overflow when n = INT_MIN
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
         }
-
-        return fastPow(x, N);
-    }
-
-private:
-    double fastPow(double x, long long n) {
-        if (n == 0) return 1.0;
-        double half = fastPow(x, n / 2);
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+      double result=1.0;
+        while(N>0){
+            if(N%2==1){
+                result*=x;
+            }
+            x*=x;
+            N/=2;
+        }return result;
+        
     }
 };
