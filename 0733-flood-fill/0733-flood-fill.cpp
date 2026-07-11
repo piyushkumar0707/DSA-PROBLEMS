d419fe2cb654902a3013b05ff9ed18e27cfd4260
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-11 20:26:03 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.78%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int color, int originalColor) {
-        int m = image.size();
-        int n = image[0].size();
 
-        // Boundary and color check
-        if (r < 0 || r >= m || c < 0 || c >= n || image[r][c] != originalColor)
-            return;
 
-        image[r][c] = color;
+    void dfs(vector<vector<int>>& image, int r, int c, int color, int originalImage){
+        int m= image.size();
+        int n= image[0].size();
+        if(r<0 || r>=m || c<0 || c>=n || image[r][c]!=originalImage)return;
+        image[r][c]=color;
+        dfs(image, r+1, c, color, originalImage);
+        dfs(image, r-1, c, color, originalImage);
+        dfs(image, r, c+1, color, originalImage);
+        dfs(image, r, c-1, color, originalImage);
 
-        // Explore 4 directions
-        dfs(image, r + 1, c, color, originalColor); // Down
-        dfs(image, r - 1, c, color, originalColor); // Up
-        dfs(image, r, c + 1, color, originalColor); // Right
-        dfs(image, r, c - 1, color, originalColor); // Left
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        if (originalColor == color) return image; // No change needed
+        int originalImage= image[sr][sc];
+        if(originalImage==color)return image;
 
-        dfs(image, sr, sc, color, originalColor);
+        dfs(image, sr, sc, color, originalImage);
         return image;
+        
     }
 };
