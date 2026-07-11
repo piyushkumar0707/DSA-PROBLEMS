deb343e9fa25569dc6fff2d45f208fd50666801d
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-11 23:30:07 +0530

    Time: 14 ms (75.48%), Space: 34.7 MB (77.53%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, 0));
-        queue<pair<pair<int,int>, int>> q;
+        int m=mat.size();
+        int n= mat[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
 
-        // Step 1: Push all 0s into queue
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i,j},0});
-                    vis[i][j] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    dist[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
 
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-
-        // Step 2: BFS
+        int dx[]={0,0,1,-1};
+        int dy[]={1,-1,0,0};
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int steps = q.front().second;
+            auto[x,y]= q.front();
             q.pop();
-            dist[row][col] = steps;
 
-            for(int i=0; i<4; i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol]){
-                    vis[nrow][ncol] = 1;
-                    q.push({{nrow, ncol}, steps + 1});
+            for(int d=0; d<4; d++){
+                int nx= x+dx[d], ny=y+dy[d];
+                if(nx>=0 && nx<m && ny>=0 && ny<n && dist[nx][ny]==-1){
+                    dist[nx][ny]= dist[x][y]+1;
+                    q.push({nx,ny});
                 }
             }
-        }
-        return dist;
+        }return dist;
+        
     }
 };
