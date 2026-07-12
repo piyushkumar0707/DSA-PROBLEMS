2c17ca2c3331c9df1d5410823ebd3c483ec5d20d
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-12 22:22:36 +0530

    Time: 0 ms (100%), Space: 13.8 MB (91.3%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,54 +1,41 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>& vis, int delrow[], int delcol[]) {
-        vis[row][col] = 1;
-        int n = board.size();
-        int m = board[0].size();
+public:
 
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O') {
-                dfs(nrow, ncol, board, vis, delrow, delcol);
-            }
-        }
+    void dfs(vector<vector<char>>& board, int r, int c){
+        int m=board.size();
+        int n=board[0].size();
+        if(r<0 || r>=m || c<0 || c>=n)return;
+        if(board[r][c]!='O')return; //agar 0 nhi to dfs nhi chalega
+        board[r][c]='S'; //safe mark
+
+        dfs(board, r+1, c);
+        dfs(board, r-1, c);
+        dfs(board, r, c+1);
+        dfs(board, r, c-1);
+
     }
 
-public:
+
+
     void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        if (n == 0) return;
-        int m = board[0].size();
-
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        // Step 1: Mark boundary-connected 'O'
-        for (int j = 0; j < m; j++) {
-            if (!vis[0][j] && board[0][j] == 'O') {
-                dfs(0, j, board, vis, delrow, delcol);
-            }
-            if (!vis[n - 1][j] && board[n - 1][j] == 'O') {
-                dfs(n - 1, j, board, vis, delrow, delcol);
-            }
+        int m= board.size();
+        int n=board[0].size();
+        for(int i=0; i<m; i++){
+            if(board[i][0]=='O')dfs(board, i,0);
+            if(board[i][n-1]=='O') dfs(board, i, n-1);
         }
-        for (int i = 0; i < n; i++) {
-            if (!vis[i][0] && board[i][0] == 'O') {
-                dfs(i, 0, board, vis, delrow, delcol);
-            }
-            if (!vis[i][m - 1] && board[i][m - 1] == 'O') {
-                dfs(i, m - 1, board, vis, delrow, delcol);
-            }
+        for(int j=0; j<n; j++){
+            if(board[0][j]=='O') dfs(board,0,j);
+            if(board[m-1][j]=='O')dfs(board, m-1,j);
         }
 
-        // Step 2: Flip unvisited 'O' to 'X'
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]=='O')board[i][j]='X'; //flip 
+                else if(board[i][j]=='S')board[i][j]='O'; //safe restore
             }
         }
+        
     }
 };
