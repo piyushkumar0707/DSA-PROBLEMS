c649f3030616071a4a39f7de29a239274a3347dc
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-14 22:12:43 +0530

    Time: 57 ms (59.1%), Space: 21.3 MB (50.55%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        if (wordSet.find(endWord) == wordSet.end()) return 0;
-
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
-
-        while (!q.empty()) {
-            auto [word, length] = q.front();
-            q.pop();
-
-            for (int i = 0; i < word.size(); ++i) {
-                string temp = word;
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    temp[i] = c;
-                    if (temp == endWord) return length + 1;
-                    if (wordSet.find(temp) != wordSet.end()) {
-                        q.push({temp, length + 1});
-                        wordSet.erase(temp);
-                    }
+    unordered_set<string> wordSet(wordList.begin(), wordList.end());
+    
+    // endWord list mein nahi hai
+    if(wordSet.find(endWord) == wordSet.end()) return 0;
+    
+    queue<pair<string, int>> q;
+    q.push({beginWord, 1});
+    wordSet.erase(beginWord); // visited mark
+    
+    while(!q.empty()) {
+        string word = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+        
+        // Har position pe a-z try karo
+        for(int i = 0; i < word.size(); i++) {
+            string temp = word;
+            for(char c = 'a'; c <= 'z'; c++) {
+                temp[i] = c;
+                if(temp == endWord) return steps + 1;
+                if(wordSet.find(temp) != wordSet.end()) {
+                    q.push({temp, steps + 1});
+                    wordSet.erase(temp); // visited
                 }
             }
         }
-
-        return 0;
     }
+    return 0;
+}
 };
