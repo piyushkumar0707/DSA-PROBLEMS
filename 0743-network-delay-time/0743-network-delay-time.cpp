5ce671e4710f67a9952fb0337e89f251d3729c91
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-16 21:43:14 +0530

    Time: 89 ms (38.29%), Space: 44.3 MB (68.54%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,44 +1,37 @@
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Step 1: Build the graph as an adjacency list
-        vector<vector<pair<int, int>>> graph(n + 1); // 1-indexed
-        for (const auto& edge : times) {
-            int u = edge[0], v = edge[1], w = edge[2];
-            graph[u].emplace_back(v, w);
+
+        // adjacency list bnao
+        vector<vector<pair<int, int>>>adj(n+1);
+        for(int i=0; i<times.size(); i++){
+            adj[times[i][0]].push_back({times[i][1], times[i][2]});
         }
 
-        // Step 2: Min-heap priority queue for Dijkstra: (distance, node)
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        vector<int> dist(n + 1, INT_MAX);
-        dist[k] = 0;
-        pq.emplace(0, k);
+        //dijkstra
+        vector<int>dist(n+1, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>pq;
+        dist[k]=0;
+        pq.push({0,k}); //{distance, node}
 
-        // Step 3: Dijkstra's algorithm
-        while (!pq.empty()) {
-            auto [time, node] = pq.top(); pq.pop();
-            if (time > dist[node]) continue;
-
-            for (auto& [nei, wt] : graph[node]) {
-                if (dist[nei] > time + wt) {
-                    dist[nei] = time + wt;
-                    pq.emplace(dist[nei], nei);
+        while(!pq.empty()){
+            auto[d, node]= pq.top(); pq.pop();
+            if(d> dist[node])continue;
+            for(int i=0; i<adj[node].size(); i++){
+                int nbr=adj[node][i].first;
+                int wt= adj[node][i].second;
+                if( dist[node]+ wt < dist[nbr]){
+                    dist[nbr]= dist[node]+ wt;
+                    pq.push({dist[nbr], nbr});
                 }
             }
         }
-
-        // Step 4: Find the maximum time to reach all nodes
-        int maxTime = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (dist[i] == INT_MAX) return -1; // unreachable node
-            maxTime = max(maxTime, dist[i]);
+        //sabse door wla node
+        int maxDist=0;
+        for(int i=1; i<=n; i++){
+            if(dist[i]==INT_MAX)return -1; //koi node unreachable
+            maxDist=max(maxDist, dist[i]);
         }
-        return maxTime;
+            return maxDist;
     }
 };
