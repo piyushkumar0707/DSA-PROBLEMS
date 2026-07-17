6a2fc20ed5b37d7fc901571bb8f8ed894167a8f0
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-17 11:25:25 +0530

    Time: 0 ms (100%), Space: 17.1 MB (96.28%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Initialize distance array with infinity
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
+        vector<int> dist(n,INT_MAX);
+        dist[src]=0;
+        for(int i=0; i<=k; i++){
+            vector<int> temp=dist;
 
-        // Perform k+1 iterations of edge relaxation
-        for (int i = 0; i <= k; ++i) {
-            vector<int> temp = dist; // Copy current distances
-            for (auto& flight : flights) {
-                int u = flight[0], v = flight[1], w = flight[2];
-                if (dist[u] != INT_MAX && dist[u] + w < temp[v]) {
-                    temp[v] = dist[u] + w;
+            for(int j=0; j<flights.size(); j++){
+                int u=flights[j][0], v=flights[j][1], wt= flights[j][2];
+                if(dist[u]!=INT_MAX && dist[u]+wt <temp[v]){
+                    temp[v]= dist[u]+wt;
                 }
-            }
-            dist = temp; // Update distances
-        }
-
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+            }dist=temp;
+        }return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
