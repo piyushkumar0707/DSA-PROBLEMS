76cec62fecf36c7403d0cf93674f9d42f236577d
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-29 15:02:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (98.38%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int maxCount=1;
+        int count=1;
+        for(int i=1; i<s.size(); i++){
+            if(s[i]==s[i-1])count++;
+            
+            else count=1;
+            maxCount= max(maxCount, count);
+        }
+        return maxCount;
+    }
+};
