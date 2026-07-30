a8fc75ef41ddd3edd2e56d9a383a960731b2776b
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-30 13:37:26 +0530

    Time: 0 ms (100%), Space: 9.5 MB (92.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return false;
-        unordered_map<char,int>freq;
-        for(char c:s)freq[c]++;
-        for(char c:t){
-            if(freq.find(c)==freq.end())return false;
-            freq[c]--;
-            if(freq[c]<0)return false;
-
+        int freq[26]={0};
+        for(int i=0; i<s.size(); i++)freq[s[i]-'a']++;
+        for(int i=0; i<t.size(); i++)freq[t[i]-'a']--;
+        for(int i=0; i<26; i++){
+            if(freq[i]!=0)return false;
         }return true;
     }
 };
