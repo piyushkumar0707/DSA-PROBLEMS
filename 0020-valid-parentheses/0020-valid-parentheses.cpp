2035b7c275f10cc392f82dab0c41b0286011b2b4
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-31 13:35:22 +0530

    Time: 3 ms (9.07%), Space: 10.1 MB (6.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,11 +6,11 @@ public:
             if(ch=='(' || ch=='{' || ch=='[')st.push(ch);
             else{
                 if(st.empty())return false;
-                char top= st.top();
-                if((ch==')' && top!='('  )||(ch=='}' && top!='{'  ) ||(ch==']' && top!='['  ))return false;
+                char top=st.top();
+                if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='['))return false;
                 st.pop();
+
             }
-        }
-        return st.empty();
+        }return st.empty();
     }
 };
