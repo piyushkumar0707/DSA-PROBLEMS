fdbc9a7ea949ed1cd2474959b5e38feb66148906
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-31 14:14:48 +0530

    Time: 3 ms (66.36%), Space: 15 MB (12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,9 +5,9 @@ public:
         map<int,int>mpp;
         for(int i=0; i<n; i++){
             int num=nums[i];
-            int moreNeeded=target-num;
+            int moreNeeded= target-num;
             if(mpp.find(moreNeeded)!=mpp.end()){
-                return{mpp[moreNeeded],i};
+                return {mpp[moreNeeded],i};
             }mpp[num]=i;
         }return {-1,-1};
     }
