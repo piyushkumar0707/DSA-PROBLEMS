f2affc1db9c5001e114e4be948ea16dad291f074
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-31 23:11:03 +0530

    Time: 39 ms (72.34%), Space: 45.5 MB (56.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>prefixCount;
-        prefixCount[0]=1; //base case;
+        unordered_map<int, int>prefixCount;
         int sum=0, count=0;
+        prefixCount[0]=1;
         for(int num:nums){
             sum+=num;
             if(prefixCount.find(sum-k)!=prefixCount.end()){
                 count+=prefixCount[sum-k];
+
             }prefixCount[sum]++;
+
         }return count;
+
+        
     }
 };
