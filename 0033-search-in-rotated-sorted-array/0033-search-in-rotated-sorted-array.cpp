ea4b250698cd6e5c698e9ed8e1c0124096224ced
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-07-31 22:43:15 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,13 +4,13 @@ public:
         int lo=0, hi=nums.size()-1;
         while(lo<=hi){
             int mid= lo+(hi-lo)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[lo]<=nums[mid]){
-                if(nums[lo]<=target & target<nums[mid])hi=mid-1;
+            if(target==nums[mid])return mid;
+            if(nums[mid]>=nums[lo]){
+                if(target<nums[mid]&& target>=nums[lo])hi=mid-1;
                 else lo=mid+1;
             }else{
-                if(nums[hi]>=target &target>nums[mid])lo=mid+1;
-                else hi=mid-1;
+               if(target>nums[mid]&& target<=nums[hi])lo=mid+1;
+               else hi=mid-1;
             }
         }return -1;
         
