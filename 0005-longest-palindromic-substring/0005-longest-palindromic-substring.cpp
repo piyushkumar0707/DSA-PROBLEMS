0f9761f94be259e545b76309ce892d7c23b4b9f7
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-08-01 10:07:21 +0530

    Time: 5 ms (90.5%), Space: 9.3 MB (77.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
 
-
     int start=0;
     int maxLength=0;
     void expandAroundCenter(string &s, int left, int right){
         while(left>=0 && right<=s.size() && s[left]==s[right]){
             left--;
             right++;
-        }
-        int length=right-left-1;
-        
-        if(length>maxLength){
+        }int length= right-left-1;
+        if(maxLength<length){
             maxLength=length;
             start=left+1;
         }
+
     }
 
+
     string longestPalindrome(string s) {
-        int n=s.size();
+        int n= s.size();
         for(int i=0; i<n; i++){
             expandAroundCenter(s,i,i);
             expandAroundCenter(s,i,i+1);
-        }
-        return s.substr(start, maxLength);
+        }return s.substr(start, maxLength);
         
     }
 };
