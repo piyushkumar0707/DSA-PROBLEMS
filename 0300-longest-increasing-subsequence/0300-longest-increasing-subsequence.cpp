11e4e6630cda60047effc72d20d4487eb7e7a0ba
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-08-01 13:43:02 +0530

    Time: 572 ms (13.06%), Space: 296.6 MB (13.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int lengthOfLIS(std::vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n, vector<int>(n+1,-1));
+        return solve(0, -1, nums,dp);
+        
+    }
 
-        int n = nums.size();
-        std::vector<int> dp(n, 1);
+    int solve(int i, int prevIndex, vector<int>&nums, vector<vector<int>>&dp){
+        if(i==nums.size())return 0;
+        if(dp[i][prevIndex+1]!=-1)return dp[i][prevIndex+1];
+        int exclude= solve(i+1, prevIndex, nums,dp);
 
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
-                }
-            }
-        }
-
-        return *std::max_element(dp.begin(), dp.end());
+        int include=0;
+        if(prevIndex ==-1 || nums[i]>nums[prevIndex]){
+            include= 1+ solve(i+1, i, nums,dp);
+        }return dp[i][prevIndex+1]=max(include, exclude);
     }
 };
+
+
+// //int lengthOfLIS(vector<int>& nums) {
+//     vector<int> tails;
+    
+//     for (int num : nums) {
+//         int pos = lower_bound(tails.begin(), tails.end(), num) - tails.begin();
+        
+//         if (pos == tails.size()) {
+//             tails.push_back(num);   // num sabse bada hai, naya length banao
+//         } else {
+//             tails[pos] = num;       // replace karo better (chhoti) ending se
+//         }
+//     }
+    
+//     return tails.size();
+// }
