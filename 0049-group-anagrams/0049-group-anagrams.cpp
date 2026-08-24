e5eb6531d1d8b002221edfdb7570707e57548498
PIYUSH KUMAR SINGH <121piyush466mits@gmail.com>
2026-08-24 19:14:47 +0530

    Time: 15 ms (69.43%), Space: 25.1 MB (71.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>>mp;
-        for(string s: strs){
-            string key= s;
-            sort(key.begin(), key.end());
-            mp[key].push_back(s);
+        unordered_map<string, vector<string>>mpp;
+       for(string& s: strs){
+        string key=s;
+        sort(key.begin(), key.end());
+        mpp[key].push_back(s);
 
-        }vector<vector<string>> result;
-    for (auto& pair : mp) {
-        result.push_back(pair.second); 
-    }return result;
+       }
+        vector<vector<string>> res;
+        for(auto& [key,group]: mpp){
+            res.push_back(group);
+        }
+        return res;
+
+
     }
 };
